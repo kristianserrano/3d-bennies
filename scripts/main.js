9f6cb895e8747f3f0631e74164df472f3e377264
Kristian Serrano <kristianserrano@gmail.com>
2022-11-21 16:52:25 -0500

    Adds setting that allows GM to control the Benny.

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,22 @@
 const moduleName = 'swade-more-bennies';
 
 Hooks.once('diceSoNiceReady', (dice3d) => {
-  const separator = `¤`;  
-  
+
   /* Bennies List
   Boxing Glove
   Bullet
-  Bullet - Animated   
+  Bullet - Animated
   Bullet Case
   Capybara
   Cheese
   Coin
   Crystal Heart
-  Crystal Heart - Animated  
-  Crystal Heart 2 - Animated  
+  Crystal Heart - Animated
+  Crystal Heart 2 - Animated
   Dynamite
   Gear
   Goblin
-  Grenade  
+  Grenade
   Grenade 2
   Helmet - World War
   Horseshoe
@@ -27,173 +26,209 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
   Shard - Blue
   Shard - Dark Blue
   Shard - Green
-  Shard - Pink 
+  Shard - Pink
   Shard - Yellow
-  Skull  
+  Skull
   Ring - That One
-  UFO  
+  UFO
   */
+
   const models = [
     {
       id: "benny-boxing_glove",
-      name: `${separator}  Boxing Glove`,
-      fileName: 'boxing_glove.glb'      
-    },    
+      name: 'Boxing Glove',
+      fileName: 'boxing_glove.glb'
+    },
     {
       id: "benny-bomb",
-      name: `${separator} Bomb - Animated`,
-      fileName: 'bomb.glb'      
-    },  
+      name: 'Bomb - Animated',
+      fileName: 'bomb.glb'
+    },
     {
       id: "benny-bullet",
-      name: `${separator} Bullet`,
-      fileName: 'bullet.glb'      
+      name: 'Bullet',
+      fileName: 'bullet.glb'
     },
     {
       id: "benny-animated-bullet",
-      name: `${separator} Bullet - Animated`,
-      fileName: 'bullet_animated.glb'      
+      name: 'Bullet - Animated',
+      fileName: 'bullet_animated.glb'
     },
     {
       id: "benny-bullet-case",
-      name: `${separator} Bullet Case`,
-      fileName: 'bullet_case.glb'      
+      name: 'Bullet Case',
+      fileName: 'bullet_case.glb'
     },
     {
       id: "benny-capybara",
-      name: `${separator} Capybara`,
-      fileName: 'capybara.glb'      
-    },        
+      name: 'Capybara',
+      fileName: 'capybara.glb'
+    },
     {
       id: "benny-cheese",
-      name: `${separator} Cheese`,
-      fileName: 'cheese.glb'      
-    },    
+      name: 'Cheese',
+      fileName: 'cheese.glb'
+    },
     {
       id: "benny-coin",
-      name: `${separator} Coin`,
-      fileName: 'coin.glb'      
-    },    
+      name: 'Coin',
+      fileName: 'coin.glb'
+    },
     {
       id: "benny-crystal-heart",
-      name: `${separator} Crystal Heart`,
-      fileName: 'crystal_heart.glb'      
+      name: 'Crystal Heart',
+      fileName: 'crystal_heart.glb'
     },
     {
       id: "benny-crystal_heart-animated",
-      name: `${separator} Crystal Heart - Animated`,
-      fileName: 'crystal_heart_animated.glb'      
+      name: 'Crystal Heart - Animated',
+      fileName: 'crystal_heart_animated.glb'
     },
     {
       id: "benny-crystal_heart2-animated",
-      name: `${separator} Crystal Heart 2 - Animated`,
-      fileName: 'crystal_heart_2.glb'      
-    },    
+      name: 'Crystal Heart 2 - Animated',
+      fileName: 'crystal_heart_2.glb'
+    },
     {
       id: "benny-dynamite",
-      name: `${separator} Dynamite`,
-      fileName: 'dynamite.glb'      
-    },    
+      name: 'Dynamite',
+      fileName: 'dynamite.glb'
+    },
     {
       id: "benny-gear",
-      name: `${separator} Gear`,
-      fileName: 'gear.glb'      
+      name: 'Gear',
+      fileName: 'gear.glb'
     },
     {
       id: "benny-goblin",
-      name: `${separator} Goblin`,
-      fileName: 'goblin.glb'      
+      name: 'Goblin',
+      fileName: 'goblin.glb'
     },
     {
       id: "benny-grenade",
-      name: `${separator} Grenade`,
-      fileName: 'grenade.glb'      
+      name: 'Grenade',
+      fileName: 'grenade.glb'
     },
     {
       id: "benny-grenade2",
-      name: `${separator} Grenade 2`,
-      fileName: 'grenade2.glb'      
+      name: 'Grenade 2',
+      fileName: 'grenade2.glb'
     },
     {
       id: "benny-helmet-world-war",
-      name: `${separator} Helmet - World War`,
-      fileName: 'helmet.glb'      
-    },    
+      name: 'Helmet - World War',
+      fileName: 'helmet.glb'
+    },
     {
       id: "benny-horseshoe",
-      name: `${separator} Horseshoe`,
-      fileName: 'horseshoe.glb'      
-    },        
+      name: 'Horseshoe',
+      fileName: 'horseshoe.glb'
+    },
     {
       id: "benny-mushroom",
-      name: `${separator} Mushroom`,
-      fileName: 'mushroom.glb'      
-    },    
+      name: 'Mushroom',
+      fileName: 'mushroom.glb'
+    },
     {
       id: "benny-pow",
-      name: `${separator} POW`,
-      fileName: 'pow.glb'      
-    },    
+      name: 'POW',
+      fileName: 'pow.glb'
+    },
     {
       id: "benny-scifi-card",
-      name: `${separator} Sci-Fi Card`,
-      fileName: 'scifi_card.glb'      
+      name: 'Sci-Fi Card',
+      fileName: 'scifi_card.glb'
     },
     {
       id: "benny-grenade2",
-      name: `${separator} Grenade 2`,
-      fileName: 'grenade2.glb'      
+      name: 'Grenade 2',
+      fileName: 'grenade2.glb'
     },
     {
       id: "benny-shard-pink",
-      name: `${separator} Shard - Pink`,
-      fileName: 'shard_pink.glb'      
+      name: 'Shard - Pink',
+      fileName: 'shard_pink.glb'
     },
     {
       id: "benny-shard-blue",
-      name: `${separator} Shard - Blue`,
-      fileName: 'shard_blue.glb'      
+      name: 'Shard - Blue',
+      fileName: 'shard_blue.glb'
     },
     {
       id: "benny-shard-dark-blue",
-      name: `${separator} Shard - Dark Blue`,
-      fileName: 'shard_dark_blue.glb'      
+      name: 'Shard - Dark Blue',
+      fileName: 'shard_dark_blue.glb'
     },
     {
       id: "benny-shard-yellow",
-      name: `${separator} Shard - Yellow`,
-      fileName: 'shard_yellow.glb'      
+      name: 'Shard - Yellow',
+      fileName: 'shard_yellow.glb'
     },
     {
       id: "benny-shard-green",
-      name: `${separator} Shard - Green`,
-      fileName: 'shard_green.glb'      
-    },           
+      name: 'Shard - Green',
+      fileName: 'shard_green.glb'
+    },
     {
       id: "benny-skull",
-      name: `${separator} Skull`,
-      fileName: 'skull.glb'      
-    },   
+      name: 'Skull',
+      fileName: 'skull.glb'
+    },
     {
       id: "one-ring",
-      name: `${separator} Ring - That One`,
-      fileName: 'one-ring.glb'      
-    },    
+      name: 'Ring - That One',
+      fileName: 'one-ring.glb'
+    },
     {
       id: "benny-ufo",
-      name: `${separator} UFO - Animated`,
-      fileName: 'ufo.glb'      
+      name: 'UFO - Animated',
+      fileName: 'ufo.glb'
     }
   ];
-  
-  for (let model of models) {
-    dice3d.addSystem({id: model.id, name: model.name }, false);
-    
-    dice3d.addDicePreset({
-      type:"db",
-      system: model.id, 
-      modelFile: 'modules/' + moduleName + '/models/' + model.fileName    
-    });
+
+  const choices = {};
+  for (const model of models) {
+    choices[model.id] = model.name;
   }
 
+  game.settings.register(moduleName, 'selectedBenny', {
+    name: "Select a Benny",
+    hint: "Choose a Benny to use for this world.",
+    scope: "world",
+    config: true,
+    requiresReload: true,
+    type: String,
+    choices: choices,
+    default: "",
+    onChange: async (value) => {
+      const currentModelID = game.settings.get(moduleName, 'selectedBenny');
+      const newModel = models.find((m) => m.id === currentModelID);
+      if (currentModelID) {
+        delete game.dice3d.DiceFactory.systems[currentModelID];
+      }
+      dice3d.addSystem({ id: newModel.id, name: newModel.name }, false);
+      dice3d.addDicePreset({
+        type: "db",
+        system: newModel.id,
+        modelFile: 'modules/' + moduleName + '/models/' + newModel.fileName
+      });
+
+      for (const user of game.users) {
+        await user.unsetFlag("dice-so-nice", "appearance");
+        await user.setFlag('dice-so-nice', 'appearance', { 'db.system': value });
+      }
+    }
+  });
+
+  // Get the currently selected model and register it.
+  const modelId = game.settings.get(moduleName, 'selectedBenny');
+  if (modelId) {
+    const model = models.find((m) => m.id === modelId);
+    dice3d.addSystem({ id: model.id, name: model.name }, false);
+    dice3d.addDicePreset({
+      type: "db",
+      system: model.id,
+      modelFile: 'modules/' + moduleName + '/models/' + model.fileName
+    });
+  }
 });
